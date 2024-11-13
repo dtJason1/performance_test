63585d035826cf46fd6f6068432f040f5b36059b
jason <originalkimchi100@gmail.com>
2024-11-13 15:11:53 +0900

    first

--- lib/main.dart
+++ lib/main.dart
@@ -59,7 +59,25 @@ class _MyHomePageState extends State<MyHomePage> {
     'assets/image/image_8.png',
     'assets/image/image_9.jpg',
     'assets/image/image_10.PNG',
-
+    'assets/image/image_1.jpg',
+    'assets/image/image_2.png',
+    'assets/image/image_3.png',
+    'assets/image/image_4.jpg',
+    'assets/image/image_5.PNG',
+    'assets/image/image_6.PNG',
+    'assets/image/image_7.png',
+    'assets/image/image_8.png',
+    'assets/image/image_9.jpg',
+    'assets/image/image_10.PNG',    'assets/image/image_1.jpg',
+    'assets/image/image_2.png',
+    'assets/image/image_3.png',
+    'assets/image/image_4.jpg',
+    'assets/image/image_5.PNG',
+    'assets/image/image_6.PNG',
+    'assets/image/image_7.png',
+    'assets/image/image_8.png',
+    'assets/image/image_9.jpg',
+    'assets/image/image_10.PNG',
   ];
 
   int currentIndex = 0; // 현재 이미지 인덱스
