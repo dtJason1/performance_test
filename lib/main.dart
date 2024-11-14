a066b1c4db00a16d470d8eab01b27895987bf863
jason <originalkimchi100@gmail.com>
2024-11-14 15:54:24 +0900

    first

--- lib/main.dart
+++ lib/main.dart
@@ -1,54 +1,73 @@
 import 'package:flutter/material.dart';
-import 'dart:async';
 
 void main() {
-  runApp(MyApp());
+  runApp(MaterialApp(
+    home: Page1(),
+  ));
 }
 
-class MyApp extends StatelessWidget {
+class Page1 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: FrequentSetStatePage(),
+    return Scaffold(
+      appBar: AppBar(title: Text('Page 1')),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.of(context).push(MaterialPageRoute(
+              builder: (context) => Page2(),
+            ));
+          },
+          child: Text('Navigate to Page 2'),
+        ),
+      ),
     );
   }
 }
 
-class FrequentSetStatePage extends StatefulWidget {
-  @override
-  _FrequentSetStatePageState createState() => _FrequentSetStatePageState();
-}
-
-class _FrequentSetStatePageState extends State<FrequentSetStatePage> {
-  int _counter = 0;
-  late Timer _timer;
-
-  @override
-  void initState() {
-    super.initState();
-    // 10ms마다 setState()를 호출하는 타이머
-    _timer = Timer.periodic(Duration(milliseconds: 10), (timer) {
-      setState(() {
-        _counter++;
-      });
-    });
-  }
-
-  @override
-  void dispose() {
-    _timer.cancel();
-    super.dispose();
-  }
-
+class Page2 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Frequent setState Example')),
+      appBar: AppBar(title: Text('Page 2')),
       body: Center(
-        child: Text(
-          'Counter: $_counter',
-          style: TextStyle(fontSize: 24),
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.of(context).push(MaterialPageRoute(
+              builder: (context) => ImagePage(),
+            ));
+          },
+          child: Text('Navigate to Image Page'),
         ),
+      ),
+    );
+  }
+}
+
+class ImagePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Image Page')),
+      body: GridView.builder(
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
+        itemCount: 10,
+        itemBuilder: (context, index) {
+          // 750x750 크기의 이미지 로드
+          return Image.asset(
+            'assets/image/image_$index.png',
+            // cacheWidth, cacheHeight 속성 생략하여 메모리 사용량이 증가할 수 있음
+          );
+        },
+      ),
+      floatingActionButton: ElevatedButton(
+        onPressed: () {
+          // 무한 네비게이션으로 인해 페이지가 계속 쌓일 수 있음
+          Navigator.of(context).push(MaterialPageRoute(
+            builder: (context) => Page1(),
+          ));
+        },
+        child: Text('Navigate to Page 1'),
       ),
     );
   }
