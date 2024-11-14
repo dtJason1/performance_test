827306debdaef495a6a17fffa8b52fd53a821815
jason <originalkimchi100@gmail.com>
2024-11-14 16:44:40 +0900

    first

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
+import 'dart:async';
 
 void main() {
   runApp(MaterialApp(
-    showPerformanceOverlay: true,
     home: Page1(),
   ));
 }
@@ -12,25 +12,6 @@ class Page1 extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Page 1')),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            Navigator.of(context).push(MaterialPageRoute(
-              builder: (context) => Page2(),
-            ));
-          },
-          child: Text('Navigate to Page 2'),
-        ),
-      ),
-    );
-  }
-}
-
-class Page2 extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text('Page 2')),
       body: Center(
         child: ElevatedButton(
           onPressed: () {
@@ -45,38 +26,64 @@ class Page2 extends StatelessWidget {
   }
 }
 
-class ImagePage extends StatelessWidget {
+class ImagePage extends StatefulWidget {
+  @override
+  _ImagePageState createState() => _ImagePageState();
+}
+
+class _ImagePageState extends State<ImagePage> with TickerProviderStateMixin {
+  List<Timer> _timers = [];
+  List<AnimationController> _controllers = [];
+
+  @override
+  void initState() {
+    super.initState();
+
+    // 여러 개의 Timer와 AnimationController 생성
+    for (int i = 0; i < 100; i++) {
+      Timer timer = Timer.periodic(Duration(milliseconds: 100), (timer) {
+        print('Timer tick $i');
+      });
+      _timers.add(timer);
+
+      AnimationController controller = AnimationController(
+        duration: Duration(seconds: 2),
+        vsync: this,
+      )..repeat();
+      _controllers.add(controller);
+    }
+  }
+
+  // dispose()에서 리소스를 해제하지 않음으로써 메모리 누수 유발
+  @override
+  void dispose() {
+    // 주석 상태로 남겨서 의도적으로 메모리 누수 발생
+    // _timers.forEach((timer) => timer.cancel());
+    // _controllers.forEach((controller) => controller.dispose());
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Image Page')),
-      body: Column(
-        children: [
-          Expanded(
-            child: ListView.builder(
-              itemCount: 10,
-              itemBuilder: (context, index) {
-                // 750x750 크기의 이미지 로드
-                return Image.asset(
-                  'assets/image/image_$index.png',
-                  // cacheWidth, cacheHeight 속성 생략하여 메모리 사용량이 증가할 수 있음
-                );
-              },
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: ElevatedButton(
-              onPressed: () {
-                // 무한 네비게이션으로 인해 페이지가 계속 쌓일 수 있음
-                Navigator.of(context).push(MaterialPageRoute(
-                  builder: (context) => Page1(),
-                ));
-              },
-              child: Text('Navigate to Page 1'),
-            ),
-          ),
-        ],
+      body: ListView.builder(
+        itemCount: 1000,
+        itemBuilder: (context, index) {
+          // 큰 이미지 로드 (반복적이고 메모리 관리 없음)
+          return Image.asset(
+            'assets/image/image_${index%10}.png',
+            // 메모리 최적화 속성 생략
+          );
+        },
+      ),
+      floatingActionButton: ElevatedButton(
+        onPressed: () {
+          Navigator.of(context).push(MaterialPageRoute(
+            builder: (context) => Page1(),
+          ));
+        },
+        child: Text('Navigate to Page 1'),
       ),
     );
   }
